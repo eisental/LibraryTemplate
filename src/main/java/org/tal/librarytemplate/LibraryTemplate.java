f7a24c7097c7d2067d4d3ad2b0749882c9214eff
Tal Eisenberg <eisental@gmail.com>
2011-02-04 23:03:07 +0100

    LibraryTemplate

---
+++ src/main/java/org/tal/librarytemplate/LibraryTemplate.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.tal.librarytemplate;
+
+import java.io.File;
+import org.bukkit.Server;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.tal.redstonechips.CircuitLibrary;
+
+/**
+ *
+ * @author Tal Eisenberg
+ */
+public class LibraryTemplate extends CircuitLibrary {
+
+    public LibraryTemplate(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
+        super(pluginLoader, instance, desc, folder, plugin, cLoader);
+
+        // Any library-wide initialization code should go here.
+    }
+
+    public Class[] getCircuitClasses() {
+        return new Class[] { test.class };
+    }
+
+    public void onEnable() {
+        // Called when the plugin is enabled.
+        // No need to write anything to the console. RedstoneChips will list the loaded circuits.
+    }
+
+    public void onDisable() {
+        // Called when the plugin is disabled.
+
+    }
+
+}
---
+++ src/main/java/org/tal/librarytemplate/test.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.tal.librarytemplate;
+
+import org.bukkit.entity.Player;
+import org.tal.redstonechips.circuit.Circuit;
+
+/**
+ *
+ * @author Tal Eisenberg
+ */
+public class test extends Circuit {
+
+    public void inputChange(int index, boolean state) {
+        // This is where you handle changes in input state.
+        // The index is the input's pin number and state is true when the input turns high and false when it turns low.
+
+        // sets the lever position of the matching output pin according to the new state.
+        sendOutput(index, state);
+    }
+
+    protected boolean init(Player player, String[] strings) {
+        // This code executes when you right-click on the circuit's sign.
+        player.sendMessage("You have activated LibraryTemplate test circuit.");
+
+        return true;
+    }
+
+}
