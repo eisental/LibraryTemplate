75dbd2fc0e3e856bebca1b5d56fc6c758dc47bc9
Tal Eisenberg <eisental@gmail.com>
2011-02-06 01:43:43 +0100

    Example of adding a circuit preference key

--- src/main/java/org/tal/librarytemplate/LibraryTemplate.java
+++ src/main/java/org/tal/librarytemplate/LibraryTemplate.java
@@ -6,7 +6,6 @@
 package org.tal.librarytemplate;
 
 import java.io.File;
-import org.bukkit.DyeColor;
 import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -22,8 +21,6 @@ public class LibraryTemplate extends CircuitLibrary {
 
     public LibraryTemplate(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
         super(pluginLoader, instance, desc, folder, plugin, cLoader);
-
-        // Any library-wide initialization code should go here.
     }
 
     public Class[] getCircuitClasses() {
@@ -31,21 +28,38 @@ public class LibraryTemplate extends CircuitLibrary {
     }
 
     public void onEnable() {
-        // Called when the plugin is enabled.
+        // Called when the circuit library plugin is enabled.
         // No need to write anything to the console. RedstoneChips will list the loaded circuits.
     }
 
     public void onDisable() {
-        // Called when the plugin is disabled.
+        // Called when the circuit library plugin is disabled.
 
+    }
+
+    public void onRedstoneChipsEnable() {
+
+        // Called by the RedstoneChips plugin immediately after finishing loading circuit classes from every installed circuit library.
+        // This is a good place to add circuit preferences keys and any other library-wide initialization code.
+
+        // The next line adds a preference key, named "test.msg" with a default value of "TEST!"
+        // The preference value can be changed from inside the game by using the /redchips-prefs test.msg <newvalue> command
+        // or by directly editing the preferences.yml file.
+        redstoneChips.getPrefsManager().registerCircuitPreference(test.class, "msg", "TEST!");
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
         // Using the usual bukkit java plugin method to handle chat commands
         if (cmd.getName().equalsIgnoreCase("redchips-test")) {
-            sender.sendMessage(redstoneChips.getPrefsManager().getInfoColor() + "TEST!");
-            return true;
-        } else return false;
-    }
+            
+            // Find the current value of the preference key we added in onRedstoneChipsEnable().
+            String msg = (String)redstoneChips.getPrefsManager().getPrefs().get("test.msg");
 
+            sender.sendMessage(redstoneChips.getPrefsManager().getInfoColor() + msg);
+            return true;
+
+        } else
+            return false;
+    }
 }
